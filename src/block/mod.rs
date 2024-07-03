b22fda83e37131a94fe8d95d677a7386704844d3
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-07-03 20:05:58 +0200

    derive Debug in Block struct

--- src/block/mod.rs
+++ src/block/mod.rs
@@ -1,24 +1,25 @@
+#[derive(Debug)]
 pub struct Block {
     timestamp: i64,
     pre_block_hash: String,
     hash: String,
-    transactions: Vec<Transaction>,
-    nonce: i64,
+    transactions: Vec<u64>,
+    nonce: u64,
     height: usize,
 }
 
 impl Block {
-    pub fn new_block(pre_block_hash: String, transactions: &[Transaction], height: usize) -> Block {
+    pub fn new_block(pre_block_hash: String, transactions: &[u64], height: usize) -> Block {
         let mut block = Block {
-            timestamp: crate::current_timestamp(),
+            timestamp: 0,
             pre_block_hash,
             hash: String::new(),
             transactions: transactions.to_vec(),
             nonce: 0,
             height,
         };
-        let pow = ProofOfWork::new_proof_of_work(block.clone());
-        let (nonce, hash) = pow.run();
+        // let pow = ProofOfWork::new_proof_of_work(block.clone());
+        let (nonce, hash) = (0, "".to_string()); // pow.run();
         block.nonce = nonce;
         block.hash = hash;
         block
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use blockchain_rust::block::Block;
 
 fn main() {
     let block = Block::new_block("".to_string(), &[], 0);
-    println!("Hello, world!");
+    println!("{:?}", block);
 }
