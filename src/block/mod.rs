591db80e92fc29970a38b9a9d7e66c015c093534
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-07-04 20:59:56 +0200

    implement Block

--- src/block/mod.rs
+++ src/block/mod.rs
@@ -1,15 +1,17 @@
-#[derive(Debug)]
+use crate::{ProofOfWork, Transaction};
+
+#[derive(Debug, Clone)]
 pub struct Block {
     timestamp: i64,
     pre_block_hash: String,
     hash: String,
-    transactions: Vec<u64>,
-    nonce: u64,
+    transactions: Vec<Transaction>,
+    nonce: i64,
     height: usize,
 }
 
 impl Block {
-    pub fn new_block(pre_block_hash: String, transactions: &[u64], height: usize) -> Block {
+    pub fn new_block(pre_block_hash: String, transactions: &[Transaction], height: usize) -> Block {
         let mut block = Block {
             timestamp: 0,
             pre_block_hash,
@@ -18,10 +20,47 @@ impl Block {
             nonce: 0,
             height,
         };
-        // let pow = ProofOfWork::new_proof_of_work(block.clone());
-        let (nonce, hash) = (0, "".to_string()); // pow.run();
+        let pow = ProofOfWork::new_proof_of_work(block.clone());
+        let (nonce, hash) = pow.run();
         block.nonce = nonce;
         block.hash = hash;
         block
+    }
+
+    pub fn generate_genesis_block(transaction: &Transaction) -> Block {
+        let transactions = vec![transaction.clone()];
+        return Block::new_block(String::from("None"), &transactions, 0);
+    }
+
+    pub fn hash_transactions(&self) -> Vec<u8> {
+        let mut txhashs = vec![];
+        for transaction in &self.transactions {
+            txhashs.extend(transaction.get_id());
+        }
+        crate::sha256_digest(txhashs.as_slice())
+    }
+
+    pub fn get_transactions(&self) -> &[Transaction] {
+        self.transactions.as_slice()
+    }
+
+    pub fn get_pre_block_hash(&self) -> String {
+        self.pre_block_hash.clone()
+    }
+
+    pub fn get_hash(&self) -> &str {
+        self.hash.as_str()
+    }
+
+    pub fn get_hash_bytes(&self) -> Vec<u8> {
+        self.hash.as_bytes().to_vec()
+    }
+
+    pub fn get_timestamp(&self) -> i64 {
+        self.timestamp
+    }
+
+    pub fn get_height(&self) -> usize {
+        self.height
     }
 }
