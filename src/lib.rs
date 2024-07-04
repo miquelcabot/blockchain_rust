a005a90bcee9b30ac6d5388024cfe43cdb7b46d4
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-07-04 20:59:39 +0200

    add utils module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
 pub mod block;
 pub mod proof_of_work;
 pub mod transaction;
+pub mod utils;
+
+pub use block::Block;
+pub use proof_of_work::ProofOfWork;
+pub use transaction::Transaction;
+pub use utils::sha256_digest;
