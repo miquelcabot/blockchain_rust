2de75df0332813ae79b9292720ac21492dd5169b
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-07-05 16:49:47 +0200

    move proofofwork struct to block module

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 pub mod block;
 pub mod blockchain;
 pub mod nodes;
-pub mod proof_of_work;
 pub mod server;
 pub mod transaction;
 pub mod utils;
@@ -10,7 +9,6 @@ pub mod wallet;
 pub use block::Block;
 pub use blockchain::Blockchain;
 pub use nodes::Nodes;
-pub use proof_of_work::ProofOfWork;
 pub use server::Server;
 pub use transaction::Transaction;
 pub use wallet::Wallet;
