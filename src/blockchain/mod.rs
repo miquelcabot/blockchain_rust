e4afb12e1099de76be95f48e2b5a27464b4e6a1e
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-07-06 09:04:16 +0200

    add BlockchainIterator struct

--- src/blockchain/mod.rs
+++ src/blockchain/mod.rs
@@ -74,4 +74,33 @@ impl Blockchain {
         let mut tip_hash = self.tip_hash.write().unwrap();
         *tip_hash = String::from(new_tip_hash)
     }
+
+    pub fn iterator(&self) -> BlockchainIterator {
+        BlockchainIterator::new(self.get_tip_hash(), self.db.clone())
+    }
+}
+
+pub struct BlockchainIterator {
+    db: Db,
+    current_hash: String,
+}
+
+impl BlockchainIterator {
+    fn new(tip_hash: String, db: Db) -> BlockchainIterator {
+        BlockchainIterator {
+            current_hash: tip_hash,
+            db,
+        }
+    }
+
+    pub fn next(&mut self) -> Option<Block> {
+        let block_tree = self.db.open_tree(BLOCKS_TREE).unwrap();
+        let data = block_tree.get(self.current_hash.clone()).unwrap();
+        if data.is_none() {
+            return None;
+        }
+        let block = Block::deserialize(data.unwrap().to_vec().as_slice());
+        self.current_hash = block.get_pre_block_hash().clone();
+        return Some(block);
+    }
 }
