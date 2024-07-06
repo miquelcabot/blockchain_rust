51e570b2e837d5d6218a7caea8e460ec9a8499f8
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-07-06 18:13:48 +0200

    add wallet implemetation

--- src/wallet/mod.rs
+++ src/wallet/mod.rs
@@ -1,3 +1,8 @@
+use serde::{Deserialize, Serialize};
+
+pub const ADDRESS_CHECK_SUM_LEN: usize = 4;
+
+#[derive(Clone, Serialize, Deserialize)]
 pub struct Wallet {
     pkcs8: Vec<u8>,
     public_key: Vec<u8>,
