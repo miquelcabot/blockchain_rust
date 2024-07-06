c8f54bfd21809abf4bfe562e3fb6ed2a57838aa9
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-07-06 18:13:43 +0200

    add transaction implemetation

--- src/transaction/mod.rs
+++ src/transaction/mod.rs
@@ -1,18 +1,43 @@
+use crate::{base58_decode, wallet};
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Transaction {
-    id: Vec<u8>,
-    vin: Vec<TXInput>,
-    vout: Vec<TXOutput>,
-}
+const SUBSIDY: i32 = 10;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct TXInput {
     txid: Vec<u8>,
     vout: usize,
     signature: Vec<u8>,
     pub_key: Vec<u8>,
+}
+
+impl TXInput {
+    pub fn new(txid: &[u8], vout: usize) -> TXInput {
+        TXInput {
+            txid: txid.to_vec(),
+            vout,
+            signature: vec![],
+            pub_key: vec![],
+        }
+    }
+
+    pub fn get_txid(&self) -> &[u8] {
+        self.txid.as_slice()
+    }
+
+    pub fn get_vout(&self) -> usize {
+        self.vout
+    }
+
+    pub fn get_pub_key(&self) -> &[u8] {
+        self.pub_key.as_slice()
+    }
+
+    pub fn uses_key(&self, pub_key_hash: &[u8]) -> bool {
+        let locking_hash = wallet::hash_pub_key(self.pub_key.as_slice());
+        return locking_hash.eq(pub_key_hash);
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -21,8 +46,59 @@ pub struct TXOutput {
     pub_key_hash: Vec<u8>,
 }
 
+impl TXOutput {
+    pub fn new(value: i32, address: &str) -> TXOutput {
+        let mut output = TXOutput {
+            value,
+            pub_key_hash: vec![],
+        };
+        output.lock(address);
+        return output;
+    }
+
+    pub fn get_value(&self) -> i32 {
+        self.value
+    }
+
+    pub fn get_pub_key_hash(&self) -> &[u8] {
+        self.pub_key_hash.as_slice()
+    }
+
+    fn lock(&mut self, address: &str) {
+        let payload = base58_decode(address);
+        let pub_key_hash = payload[1..payload.len() - wallet::ADDRESS_CHECK_SUM_LEN].to_vec();
+        self.pub_key_hash = pub_key_hash;
+    }
+
+    pub fn is_locked_with_key(&self, pub_key_hash: &[u8]) -> bool {
+        self.pub_key_hash.eq(pub_key_hash)
+    }
+}
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct Transaction {
+    id: Vec<u8>,
+    vin: Vec<TXInput>,
+    vout: Vec<TXOutput>,
+}
+
 impl Transaction {
     pub fn get_id(&self) -> &[u8] {
         self.id.as_slice()
+    }
+
+    pub fn new_coinbase_tx(to: &str) -> Transaction {
+        let txout = TXOutput::new(SUBSIDY, to);
+        let mut tx_input = TXInput::default();
+        tx_input.signature = Uuid::new_v4().as_bytes().to_vec();
+
+        let mut tx = Transaction {
+            id: vec![],
+            vin: vec![tx_input],
+            vout: vec![txout],
+        };
+
+        tx.id = tx.hash();
+        return tx;
     }
 }
