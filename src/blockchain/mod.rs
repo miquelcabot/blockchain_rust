46e9869d4d3a48466a10e00d2532ed75fd057d5f
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-07-06 08:57:10 +0200

    implement Blockchain struct

--- src/blockchain/mod.rs
+++ src/blockchain/mod.rs
@@ -1,7 +1,64 @@
-use sled::Db;
+use crate::{Block, Transaction};
+use sled::transaction::TransactionResult;
+use sled::{Db, Tree};
+use std::env::current_dir;
 use std::sync::{Arc, RwLock};
+
+const TIP_BLOCK_HASH_KEY: &str = "tip_block_hash";
+const BLOCKS_TREE: &str = "blocks";
 
 pub struct Blockchain {
     tip_hash: Arc<RwLock<String>>, // hash of last block
     db: Db,
+}
+
+impl Blockchain {
+    /// Create a new blockchain with a genesis block.
+    /// The genesis block contains a coinbase transaction.
+    /// # Arguments
+    /// * `genesis_address` - The address designated to receive the initial reward
+    /// # Returns
+    /// * `Blockchain` - The blockchain with the genesis block
+    pub fn create_blockchain(genesis_address: &str) -> Blockchain {
+        let db = sled::open(current_dir().unwrap().join("data")).unwrap();
+        let blocks_tree = db.open_tree(BLOCKS_TREE).unwrap();
+
+        let data = blocks_tree.get(TIP_BLOCK_HASH_KEY).unwrap();
+        let tip_hash;
+        if data.is_none() {
+            let coinbase_tx = Transaction::new_coinbase_tx(genesis_address);
+            let block = Block::generate_genesis_block(&coinbase_tx);
+            Self::update_blocks_tree(&blocks_tree, &block);
+            tip_hash = String::from(block.get_hash());
+        } else {
+            tip_hash = String::from_utf8(data.unwrap().to_vec()).unwrap();
+        }
+        Blockchain {
+            tip_hash: Arc::new(RwLock::new(tip_hash)),
+            db,
+        }
+    }
+
+    fn update_blocks_tree(blocks_tree: &Tree, block: &Block) {
+        let block_hash = block.get_hash();
+        let _: TransactionResult<(), ()> = blocks_tree.transaction(|tx_db| {
+            let _ = tx_db.insert(block_hash, block.clone());
+            let _ = tx_db.insert(TIP_BLOCK_HASH_KEY, block_hash);
+            Ok(())
+        });
+    }
+
+    pub fn new_blockchain() -> Blockchain {
+        let db = sled::open(current_dir().unwrap().join("data")).unwrap();
+        let blocks_tree = db.open_tree(BLOCKS_TREE).unwrap();
+        let tip_bytes = blocks_tree
+            .get(TIP_BLOCK_HASH_KEY)
+            .unwrap()
+            .expect("No existing blockchain found. Create one first.");
+        let tip_hash = String::from_utf8(tip_bytes.to_vec()).unwrap();
+        Blockchain {
+            tip_hash: Arc::new(RwLock::new(tip_hash)),
+            db,
+        }
+    }
 }
