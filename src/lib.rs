a37f6ea3dcab4318ab3c0b9eb89d0a46e52f453f
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-07-06 18:25:11 +0200

    add hash_pub_key function

--- src/lib.rs
+++ src/lib.rs
@@ -17,4 +17,5 @@ pub use wallet::Wallet;
 pub use utils::base58_decode;
 pub use utils::base58_encode;
 pub use utils::current_timestamp;
+pub use utils::hash_pub_key;
 pub use utils::sha256_digest;
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,7 @@
+use crypto::digest::Digest;
 use bs58;
 use ring::digest::{Context, SHA256};
+use std::iter::repeat;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 pub fn current_timestamp() -> u64 {
@@ -14,6 +16,19 @@ pub fn sha256_digest(data: &[u8]) -> Vec<u8> {
     context.update(data);
     let digest = context.finish();
     digest.as_ref().to_vec()
+}
+
+fn ripemd160_digest(data: &[u8]) -> Vec<u8> {
+    let mut ripemd160 = crypto::ripemd160::Ripemd160::new();
+    ripemd160.input(data);
+    let mut buf: Vec<u8> = repeat(0).take(ripemd160.output_bytes()).collect();
+    ripemd160.result(&mut buf);
+    return buf;
+}
+
+pub fn hash_pub_key(pub_key: &[u8]) -> Vec<u8> {
+    let pub_key_sha256 = crate::sha256_digest(pub_key);
+    ripemd160_digest(pub_key_sha256.as_slice())
 }
 
 pub fn base58_encode(data: &[u8]) -> String {
