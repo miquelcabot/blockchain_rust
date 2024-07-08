0eaf4507d136fad59d90167534e4199d7d035cd7
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-07-08 16:35:55 +0200

    rename wallet to wallets module

--- src/wallet.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-pub const ADDRESS_CHECK_SUM_LEN: usize = 4;
-
-#[derive(Clone, Serialize, Deserialize)]
-pub struct Wallet {
-    pkcs8: Vec<u8>,
-    public_key: Vec<u8>,
-}
