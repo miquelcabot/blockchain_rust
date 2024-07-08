705e26ac92056e38ad7642247e80743c4e935dc1
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-07-08 10:06:25 +0200

    implement Transaction struct

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,4 +1,4 @@
-use crate::{base58_decode, wallet};
+use crate::{base58_decode, wallet, Blockchain};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
@@ -83,10 +83,6 @@ pub struct Transaction {
 }
 
 impl Transaction {
-    pub fn get_id(&self) -> &[u8] {
-        self.id.as_slice()
-    }
-
     pub fn new_coinbase_tx(to: &str) -> Transaction {
         let txout = TXOutput::new(SUBSIDY, to);
         let mut tx_input = TXInput::default();
@@ -100,5 +96,87 @@ impl Transaction {
 
         tx.id = tx.hash();
         return tx;
+    }
+
+    fn trimmed_copy(&self) -> Transaction {
+        let mut inputs = vec![];
+        let mut outputs = vec![];
+        for input in &self.vin {
+            let txinput = TXInput::new(input.get_txid(), input.get_vout());
+            inputs.push(txinput);
+        }
+        for output in &self.vout {
+            outputs.push(output.clone());
+        }
+        Transaction {
+            id: self.id.clone(),
+            vin: inputs,
+            vout: outputs,
+        }
+    }
+
+    pub fn verify(&self, blockchain: &Blockchain) -> bool {
+        if self.is_coinbase() {
+            return true;
+        }
+        let mut tx_copy = self.trimmed_copy();
+        for (idx, vin) in self.vin.iter().enumerate() {
+            let prev_tx_option = blockchain.find_transaction(vin.get_txid());
+            if prev_tx_option.is_none() {
+                panic!("ERROR: Previous transaction is not correct")
+            }
+            let prev_tx = prev_tx_option.unwrap();
+            tx_copy.vin[idx].signature = vec![];
+            tx_copy.vin[idx].pub_key = prev_tx.vout[vin.vout].pub_key_hash.clone();
+            tx_copy.id = tx_copy.hash();
+            tx_copy.vin[idx].pub_key = vec![];
+
+            let verify = crate::ecdsa_p256_sha256_sign_verify(
+                vin.pub_key.as_slice(),
+                vin.signature.as_slice(),
+                tx_copy.get_id(),
+            );
+            if !verify {
+                return false;
+            }
+        }
+        true
+    }
+
+    pub fn is_coinbase(&self) -> bool {
+        return self.vin.len() == 1 && self.vin[0].pub_key.len() == 0;
+    }
+
+    fn hash(&mut self) -> Vec<u8> {
+        let tx_copy = Transaction {
+            id: vec![],
+            vin: self.vin.clone(),
+            vout: self.vout.clone(),
+        };
+        crate::sha256_digest(tx_copy.serialize().as_slice())
+    }
+
+    pub fn get_id(&self) -> &[u8] {
+        self.id.as_slice()
+    }
+
+    pub fn get_id_bytes(&self) -> Vec<u8> {
+        self.id.clone()
+    }
+
+    pub fn get_vin(&self) -> &[TXInput] {
+        self.vin.as_slice()
+    }
+
+    pub fn get_vout(&self) -> &[TXOutput] {
+        self.vout.as_slice()
+    }
+
+    pub fn serialize(&self) -> Vec<u8> {
+        bincode::serialize(self).unwrap().to_vec()
+    }
+
+    pub fn deserialize(bytes: &[u8]) -> Transaction {
+        bincode::deserialize(bytes).unwrap()
     }
 }
