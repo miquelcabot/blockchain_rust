27136dd5728955144b9d6e8eb581863a3613e368
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-07-08 10:35:48 +0200

    add nodes module

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,37 @@
 pub mod block;
-pub mod blockchain;
-pub mod config;
-pub mod memory_pool;
-pub mod nodes;
-pub mod server;
-pub mod transaction;
-pub mod utils;
-pub mod utxo_set;
-pub mod wallet;
-
 pub use block::Block;
+
+pub mod blockchain;
 pub use blockchain::Blockchain;
+
+pub mod config;
 pub use config::Config;
 pub use config::GLOBAL_CONFIG;
+
+pub mod memory_pool;
 pub use memory_pool::BlockInTransit;
 pub use memory_pool::MemoryPool;
+
+pub mod nodes;
 pub use nodes::Nodes;
+
+pub mod server;
 pub use server::Server;
+
+pub mod transaction;
 pub use transaction::TXOutput;
 pub use transaction::Transaction;
-pub use utxo_set::UTXOSet;
-pub use wallet::Wallet;
 
-// functions from utils
+pub mod utils;
 pub use utils::base58_decode;
 pub use utils::base58_encode;
 pub use utils::current_timestamp;
 pub use utils::ecdsa_p256_sha256_sign_verify;
 pub use utils::hash_pub_key;
 pub use utils::sha256_digest;
+
+pub mod utxo_set;
+pub use utxo_set::UTXOSet;
+
+pub mod wallet;
+pub use wallet::Wallet;
